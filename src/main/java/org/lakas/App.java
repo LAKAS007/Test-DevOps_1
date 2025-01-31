917b691fd34f9ebe029e7d7a4963b914651fe1f2
lakas <VasilyevNA22@st.ithub.ru>
2025-01-31 18:59:13 +0300

    hotfix/issue_change_main_Hello_world fixed string in main

--- src/main/java/org/lakas/App.java
+++ src/main/java/org/lakas/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        FirstClass.methodForSout("Heloworld!!!");
+        FirstClass.methodForSout("Helo world!!!");
     }
 }
